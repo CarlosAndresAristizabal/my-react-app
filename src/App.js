f3e72b9d07220319dd263944e952342fa076c50d
Carlos Andres <carlosaristi22@gmail.com>
2023-05-28 15:38:07 -0500

    modificaion app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-        Hola Muchachos!
+        Hola Muchachos y las chelitas?
         </p>
         <a
           className="App-link"
